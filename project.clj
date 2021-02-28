8ecf936a64305cd6a36fdb11cda2142501e4a007
Mihael Konjevic <konjevic@gmail.com>
2021-02-28 23:04:09 +0100

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.verybigthings/penkala "0.0.2"
+(defproject com.verybigthings/penkala "0.0.3"
   :description "Composable query builder for PostgreSQL."
   :url "https://github.com/retro/penkala"
   :license {:name "MIT License"
