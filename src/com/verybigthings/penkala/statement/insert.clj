5180463b5721047a32af5a0f54ee900034db5de7
Mihael Konjevic <konjevic@gmail.com>
2021-02-28 21:25:15 +0100

    Add support for insert on conflict (upsert)

--- src/com/verybigthings/penkala/statement/insert.clj
+++ src/com/verybigthings/penkala/statement/insert.clj
@@ -16,7 +16,66 @@
   (if (:projection insertable)
     (-> acc
       (update :query conj "RETURNING")
-      (sel/with-projection env insertable))
+      (sel/with-projection env (dissoc insertable :joins)))
+    acc))
+
+(defn with-on-conflict-where [acc env insertable]
+  (if-let [where (get-in insertable [:on-conflict :where])]
+    (let [{:keys [query params]}
+          (sel/compile-value-expression sel/empty-acc env insertable where)]
+      (-> acc
+        (update :query conj "WHERE")
+        (update :query into query)
+        (update :params into params)))
+    acc))
+
+(defn with-on-conflict-updates [acc env insertable]
+  (if-let [updates (get-in insertable [:on-conflict :updates])]
+    (let [{:keys [query params]}
+          (reduce-kv
+            (fn [acc' col col-update]
+              (let [col-id   (get-in insertable [:aliases->ids col])
+                    col-name (get-in insertable [:columns col-id :name])
+                    {:keys [query params]} (sel/compile-value-expression sel/empty-acc env insertable col-update)]
+                (-> acc'
+                  (update :query conj (str/join " " (into [(q col-name) "="] query)))
+                  (update :params into params))))
+            sel/empty-acc
+            updates)]
+      (-> acc
+        (update :query into ["SET" (str/join ", " query)])
+        (update :params into params)))
+    acc))
+
+(defn with-on-conflict-conflict-target [acc env insertable]
+  (if-let [[conflict-target-type conflict-target] (get-in insertable [:on-conflict :conflict-target])]
+    (do
+      (case conflict-target-type
+        :value-expressions
+        (let [{:keys [query params]}
+              (reduce
+                (fn [acc conflict]
+                  (let [{:keys [query params]} (sel/compile-value-expression sel/empty-acc env insertable conflict)]
+                    (-> acc
+                      (update :query conj (str/join " " query))
+                      (update :params into params))))
+                sel/empty-acc
+                conflict-target)]
+          (-> acc
+            (update :query conj (str "(" (str/join ", " query) ")"))
+            (update :params into params)))
+        :on-constraint
+        (update acc :query into ["ON CONSTRAINT" conflict-target])))
+    acc))
+
+(defn with-on-conflict [acc env insertable]
+  (if-let [on-conflict (:on-conflict insertable)]
+    (let [action ({:nothing "DO NOTHING" :update "DO UPDATE"} (:action on-conflict))]
+      (-> acc
+        (update :query conj "ON CONFLICT")
+        (with-on-conflict-conflict-target env insertable)
+        (update :query conj action)
+        (with-on-conflict-updates env insertable)))
     acc))
 
 (defn get-insertable-columns [insertable data]
@@ -73,5 +132,6 @@
                                             (get-schema-qualified-relation-name env insertable)]
                                     :params []}
                                  (with-columns-and-values env insertable data)
+                                 (with-on-conflict env insertable)
                                  (with-returning env insertable))]
     (into [(str/join " " query)] params)))
