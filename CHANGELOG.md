1eccdd894dfa25e5c6572b9403e6b084e8935074
Mihael Konjevic <konjevic@gmail.com>
2022-06-08 16:57:07 +0200

    Update deps and bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,15 @@
 # Change Log
 
+## [0.9.0] - 2022-06-08
+
+- Implement CTEs
+- Implement support for ordering in aggregate functions
+- Implement support for array literal
+- Implement tests based on the queries found on https://www.postgresqltutorial.com
+- Implement explicit join API functions
+- Implement explicit GROUP BY, GROUPING SETS, CUBE and ROLLUP
+- Implement set operations in value expressions
+
 ## [0.1.0] - 2021-05-26
 
 - Penkala will now throw an exception if a keyword can't be matched to a column. Previously keyword was treated as a value which made it easy to write a query that looked correct, but had a wrong behavior.
--- project.clj
+++ project.clj
@@ -1,13 +1,14 @@
-(defproject com.verybigthings/penkala "0.1.0"
+(defproject com.verybigthings/penkala "0.9.0"
   :description "Composable query builder for PostgreSQL."
   :url "https://github.com/retro/penkala"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
   :source-paths ["src"]
-  :dependencies [[com.layerware/hugsql "0.5.1"]
-                 [com.layerware/hugsql-adapter-next-jdbc "0.5.1"]
-                 [seancorfield/next.jdbc "1.1.610"]
-                 [org.postgresql/postgresql "42.2.18"]
+  :dependencies [[com.layerware/hugsql "0.5.3"]
+                 [com.layerware/hugsql-adapter-next-jdbc "0.5.3"]
+                 [com.github.seancorfield/next.jdbc "1.2.780"]
+                 [org.postgresql/postgresql "42.3.6"]
                  [camel-snake-kebab/camel-snake-kebab "0.4.2"]
                  [com.github.vertical-blank/sql-formatter "1.0.3"]
-                 [metosin/jsonista "0.2.7"]])
+                 [metosin/jsonista "0.2.7"]
+                 [com.stuartsierra/dependency "1.0.0"]])
